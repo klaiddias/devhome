d81a4faf0d1fd5d815a46d0b379aab0b8bff3f93
klaiddias <klaiddias@gmail.com>
2020-01-07 14:16:45 -0300

    Implement Shap to resize images

---
+++ _posts/2020-01-06-promise-basico.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title:  "Promise - Entenda-as e descubra seu poder"
+author: klaid
+categories: [ JavaScript ]
+image: assets/images/promises.jpg
+---
+
+As I engage in the so-called "bull sessions" around and about the school, I too often find that most college men have a misconception of the purpose of education. Most of the "brethren" think that education should equip them with the proper instruments of exploitation so that they can forever trample over the masses. Still others think that education should furnish them with noble ends rather than means to an end.
---
+++ image-resize-sharp.js
@@ -0,0 +1,85 @@
+//Name of the File : image-resize-sharp.js
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+
+const _PATH_SOURCE = path.normalize('./assets/images_orig/');
+const _PATH_DEST_THUMB = path.normalize('./assets/thumbnail/');
+const _PATH_DEST_POST = path.normalize('./assets/images/');
+
+
+const fileThumb = {w: 348, h: 232, pathDest: _PATH_DEST_THUMB}
+const filePost = {w: 802, h: 500,  pathDest: _PATH_DEST_POST}
+
+
+fs.readdir(_PATH_SOURCE, (err, files)=> {
+    if (err) throw err;
+    let newFile;
+    files.forEach((fname) => {
+        let newName = path.join(filePost.pathDest, fname);
+        let oldName = path.join(_PATH_SOURCE, fname);
+
+        resize({w: 802, h: 500,  pathDest: _PATH_DEST_POST, newFileName: newName, oldFileName: oldName});
+        // resize({w: 802, h: 500,  pathDest: _PATH_DEST_POST, newFileName: newName, oldFileName: oldName})
+        //     .then((data) => {
+        //         console.log(data);
+        //     })
+        //     .catch((e) =>{
+        //         console.log(e);
+        //     });
+
+
+    });
+});
+
+
+function resize(fileType) {
+    // return new Promise ((resolve, reject) => {
+    //     fs.exists(fileType.newFileName, (file_exist) => {
+    //         if (file_exist) 
+    //             resolve(`${fileType.newFileName} - exist.`);
+    //         else
+    //             reject(`${fileType.newFileName} - no exist.`);
+    //     })
+    // });
+
+    
+    fs.readFile(path.normalize(fileType.newFileName), (not_found, data) => {
+        if (not_found) {
+            resizeFile(fileType)
+                .then((data) => { console.log(data);})
+                .catch(function (err) { console.log(err); });
+        } else {
+            console.log(`file exist: ${fileType.newFileName};`);
+        }
+    });
+    
+}
+
+// function mountAllFilePaths(pathSouce, fileName, fileType) {
+//     fileType.newFileName = path.join(fileType.pathDest, fileName);
+//     fileType.oldFileName = path.join(pathSouce, fileName);
+// }
+//http://sharp.pixelplumbing.com/en/stable/api-resize/#examples
+const resizeFile = function (fileType) {
+    return new Promise ((resolve, reject) => {
+        //resolve(`${oldFile}\n${newFile}`);
+        sharp(fileType.oldFileName)
+        .resize(fileType.w, fileType.h, {
+            fit: sharp.fit.inside,
+            withoutEnlargement: true
+          })
+        .toBuffer()
+        .then( data => {
+            fs.writeFileSync(fileType.newFileName, data);
+            resolve(`File resized sucess.[${fileType.newFileName}]`);
+        })
+        .catch( err => {
+            reject(`Error to resize the file [${fileType.oldFileName}] (${err})`);
+     });
+  
+        
+    });
+};
+
+
